b5f045343ee3ead1199327513f9280c341d0ccd9
bellujrb <bellujrb@gmail.com>
2023-09-30 21:18:59 -0300

    upgrade website

--- js/inicio.js
+++ js/inicio.js
@@ -198,12 +198,24 @@ publications.forEach((perfil) => {
 });
 
 const ownerPublishTextArea = document.querySelector("#owner-publish");
+const points = document.querySelector('#pontos-adquiridos')
+
+points.innerHTML = '5025xp'
+
+let initialPoints = 5025
+
+const toast = new bootstrap.Toast(document.querySelector('.toast'))
 
 function addOwnerPublish() {
   if (ownerPublishTextArea.value.trim() === "") {
     ownerPublishTextArea.classList.add("is-invalid");
     return;
   }
+
+  initialPoints += 50;
+  points.innerHTML = initialPoints + 'xp'
+
+  toast.show()
   ownerPublishTextArea.classList.remove("is-invalid");
   DOM.addOwnerPublish(ownerPublishTextArea.value);
   ownerPublishTextArea.value = "";
